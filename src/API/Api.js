099a907006c2e82e39acc9ae8c6df26822787e65
Eng-Hady <76622156+hadyronaldo2030@users.noreply.github.com>
2025-01-20 00:28:02 -0800

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,4 +1,4 @@
-export const baseURL = 'https://ecommerce-production-860a.up.railway.app//api';
+export const baseURL = 'https://ecommerce-production-860a.up.railway.app/api';
 
 // Auth
 export const REGISTER = "register";
